0043432dae96b6316ea971b72d83079661f0b637
Manin611 <samuelteby@gmail.com>
2026-04-24 15:45:23 -0500

    estructura de carpetas y enrutamiento con react-router-dom

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from 'react-router-dom'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Inicio from '../pages/Inicio'
+import Productos from '../pages/Productos'
+import Contacto from '../pages/Contacto'
+
+function AppRouter() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/productos" element={<Productos />} />
+        <Route path="/contacto" element={<Contacto />} />
+      </Routes>
+      <Footer />
+    </>
+  )
+}
+
+export default AppRouter
